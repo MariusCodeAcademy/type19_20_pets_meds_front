9f740eeed5cb73b3389f47e0ef8b451c7197fa83
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-05 12:13:48 +0200

    individual errors

--- js/add-log.js
+++ js/add-log.js
@@ -1,3 +1,4 @@
+/* eslint-disable guard-for-in */
 import { logsUrl } from './modules/helper.js';
 
 console.log('add-log.js file was loaded');
@@ -38,6 +39,7 @@ function createLog(event) {
       console.log('data ===', data);
       if (data !== 'Success') {
         showErrors(data);
+        showIndividualErorrs(data);
         return;
       }
       console.log('klaidu nera');
@@ -57,4 +59,30 @@ function showErrors(errorArr) {
     errEl.textContent = `${eObj.field}: ${eObj.error}`;
     els.errorList.append(errEl);
   });
+}
+
+function showIndividualErorrs(errorsArr) {
+  console.log('errorsArr ===', errorsArr);
+  // { status: mus be kazkas  }
+  // const errorObjArr = errorsArr.map((eObj) => {
+  //   return { [eObj.field]: eObj.error };
+  // });
+  // console.log('errorObjArr ===', errorObjArr);
+
+  // sukti cikla per objekta els
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key in els) {
+    const value = els[key];
+    console.log('key ===', key);
+    // tikrinam ar errorObjArr yra toks key
+    const found = errorsArr.find((eObj) => eObj.field === key);
+    if (found) {
+      // uzdeti klaida
+      console.log('value ===', value);
+      value.classList.add('is-invalid');
+      value.nextElementSibling.textContent = found.error;
+    }
+  }
+  // paimam viena propertie form: document.querySelector('form'),
+  // jei key yra tai value uzdededu klase ir po value atvaizduoju klaida
 }
