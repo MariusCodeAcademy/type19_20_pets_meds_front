f239e851f74ece5c4f247c838b8cff7ab6bf2ac0
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-05 09:12:50 +0200

    uzd

--- js/add-pet.js
+++ js/add-pet.js
@@ -5,6 +5,7 @@ const els = {
   name: document.getElementById('name'),
   dob: document.getElementById('dob'),
   clientEmail: document.getElementById('clientEmail'),
+  errorList: document.getElementById('errors-list'),
 };
 
 console.log('els ===', els);
@@ -14,6 +15,7 @@ els.form.addEventListener('submit', createPet);
 function createPet(event) {
   event.preventDefault();
   console.log('form submitted');
+  els.errorList.innerHTML = '';
   const petObj = {
     name: els.name.value,
     dob: els.dob.value,
@@ -31,6 +33,11 @@ function createPet(event) {
     .then((resp) => resp.json())
     .then((data) => {
       console.log('data ===', data);
+      if (data !== 'Success') {
+        showErrors(data);
+        return;
+      }
+      console.log('klaidu nera');
       // redirect to logs.html?petId=2
       // window.location = 'index.html';
     })
@@ -47,4 +54,14 @@ const errArr = [
 
 // kai yra klaidu,
 // 1. atvaizduoti visas klaidas viename klaidu bloke
+function showErrors(errorArr) {
+  // console.log('errorArr ===', errorArr);
+  // sukti cikla
+  errorArr.forEach((eObj) => {
+    const liEl = document.createElement('li');
+    liEl.textContent = eObj.error;
+    els.errorList.append(liEl);
+  });
+  // gaminti po klaidos li ir deti i klaidu ul
+}
 // 2. atvaziduoti klaida ties konkreciu inputu
